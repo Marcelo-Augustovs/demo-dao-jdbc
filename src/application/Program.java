ae7f273b0c76f88dc71b87c62c925cbe9c75995c
Marcelo Augusto <marcelo.augustovs99@gmail.com>
2023-04-10 15:42:45 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,13 +2,19 @@ package application;
 
 import entities.Department;
 import entities.Seller;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 
 import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1,"books");
+
         Seller seller = new Seller(21,"Bob","bob@gmail.com",new Date(),3000.00,obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(seller);
     }
 }
